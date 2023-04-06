92227315b441cf261b700b9f15fe41dc9faf8549
dazhi0619 <da.zhi0619@gmail.com>
2023-04-06 21:46:25 +0800

    死机

--- src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
+++ src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
@@ -1,16 +1,26 @@
 package com.sustech.cs_funding.mapper;
 
+import com.sustech.cs_funding.entity.GroupFund;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface FundingMapper {
 
-    @Select("SELECT total FROM group_funding WHERE group_name = #{group} and funding_name = #{funding}")
+    @Select("SELECT total FROM group_fund WHERE group_name = #{group} and fund_name = #{funding}")
     Integer selectTotalFunding(String group, String funding);
 
-    @Select("SELECT used FROM group_funding WHERE group_name = #{group} and funding_name = #{funding}")
+    @Select("SELECT used FROM group_fund WHERE group_name = #{group} and fund_name = #{funding}")
     Integer selectUsedFunding(String group, String funding);
     
+    @Select("SELECT * FROM group_fund WHERE group_name = #{group}")
+    List<GroupFund> selectFundingByGroup(String group);
     
+    @Select("SELECT * FROM group_fund WHERE fund_name = #{fund}")
+    List<GroupFund> selectFundingBySum(String fund);
+    
+    @Select("SELECT name FROM fund")
+    List<String> getAllFundingName();
 }
--- src/main/java/com/sustech/cs_funding/service/funding/FundingService.java
+++ src/main/java/com/sustech/cs_funding/service/funding/FundingService.java
@@ -1,16 +1,25 @@
 package com.sustech.cs_funding.service.funding;
 
 import com.sustech.cs_funding.common.Result;
+import com.sustech.cs_funding.entity.GroupFund;
+import com.sustech.cs_funding.entity.ResearchGroup;
 import com.sustech.cs_funding.mapper.FundingMapper;
+import com.sustech.cs_funding.mapper.ResearchGroupMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
 
 @Service
 public class FundingService {
     @Autowired
     FundingMapper fundingMapper;
+    
+    @Autowired
+    ResearchGroupMapper researchGroupMapper;
 
     public Result getFundingInfo(String funding, String group) {
         Integer total = fundingMapper.selectTotalFunding(group, funding);
@@ -20,5 +29,29 @@ public class FundingService {
         res.put("used", used);
         res.put("rest", total - used);
         return Result.ok().code(200).message("Success").addData("funding_info", res);
+    }
+    
+    public Result getFundingByGroup(){
+        List<ResearchGroup> groups = researchGroupMapper.selectAllResearchGroup();
+        List<String> fundingNames = fundingMapper.getAllFundingName();
+        HashMap<String, HashSet<String, HashMap<String, Double>>> res = new HashMap<>();
+        
+        for (int i = 0; i < groups.size(); i++) {
+            String group_name = groups.get(i).getName();
+            res.put(group_name,new ArrayList<>());
+            
+            for (int j = 0; j < fundingNames.size(); j++) {
+                String fund_name = fundingNames.get(j);
+                HashMap<String, Double> maps = new HashMap<>();
+                maps.put("total", groupFunds.);
+            }
+            List<GroupFund> groupFunds= fundingMapper.selectFundingByGroup(group_name);
+            
+            
+        }
+    }
+    
+    public Result getFundingBySum(){
+        
     }
 }
