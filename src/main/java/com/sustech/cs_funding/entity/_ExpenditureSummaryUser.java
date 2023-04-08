26563c453ea85cab65a3769e289d9a794440a168
Siri-Lee <109658172+Siri-Lee@users.noreply.github.com>
2023-04-08 14:40:32 +0800

    Create _ExpenditureSummaryUser.java

---
+++ src/main/java/com/sustech/cs_funding/entity/_ExpenditureSummaryUser.java
@@ -0,0 +1,15 @@
+package com.sustech.cs_funding.entity;
+import lombok.Data;
+
+@Data
+public class _ExpenditureSummaryUser {
+    String code;
+    String name;
+    String due_date;
+    Double total_sum;
+    Double used_sum;
+    Double left_sum;
+    String current_execution_rate;
+    String qualified;
+    String days_left;
+}
