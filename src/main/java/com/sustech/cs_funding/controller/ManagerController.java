a1e6842707239a1b76fa602f58b54c5d02efd7e3
Siri-Lee <109658172+Siri-Lee@users.noreply.github.com>
2023-04-08 19:03:38 +0800

    Create ManagerController.java

---
+++ src/main/java/com/sustech/cs_funding/controller/ManagerController.java
@@ -0,0 +1,28 @@
+package com.sustech.cs_funding.controller;
+
+import com.sustech.cs_funding.common.Result;
+import com.sustech.cs_funding.service.funding.FundingService;
+import com.sustech.cs_funding.service.manager.ManagerService;
+import com.sustech.cs_funding.service.research_group.ResearchGroupService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/manager")
+@CrossOrigin
+public class ManagerController {
+    @Autowired
+    ManagerService managerService;
+
+    @GetMapping("/moneyAllocation")
+    @ApiOperation(value = "Manager to allocate the money")
+    public Result moneyAllocation(String group_name, String fund_name, Double total) {
+        return managerService.moneyAllocation(group_name, fund_name, total);
+    }
+
+
+}
