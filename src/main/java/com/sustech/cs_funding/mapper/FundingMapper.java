8ed4fb9b80984104e8700e874ae64c85d04906ef
FffffeiLSM <12012939@mail.sustech.edu.cn>
2023-04-10 17:52:10 +0800

    fix query

--- src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
+++ src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
@@ -36,18 +36,13 @@ public interface FundingMapper {
     @Select("SELECT group_name, fund_name, code, total, used, (total - used) as usable_left, (100 * used / total || '%') as current_execution_rate, fund.execution_rate as qualified FROM group_fund JOIN fund ON  fund.name = group_fund.fund_name")
     List<_ExpenditureSummary> calculateExpenditureSummary();
 
-    @Select("SELECT fund.code                                                   as code,\n" +
-            "       fund.name                                                   as name,\n" +
-            "       group_fund.group_name                                       as groups,\n" +
-            "       group_fund.total                                            as total_sum,\n" +
-            "       SUM(group_fund.used)                                        as used_sum,\n" +
-            "       expense_category.first                                      as first_category,\n" +
-            "       expense_category.second                                     as second_category\n" +
-            "From application\n" +
-            "         JOIN group_fund ON group_fund.group_name = application.group_name\n" +
-            "         JOIN fund ON fund.name = group_fund.fund_name\n" +
-            "         JOIN expense_category ON expense_category.id = application.expense_category\n" +
-            "group by fund.name, group_fund.group_name, expense_category.first,expense_category.second, group_fund.total")
+    @Select("SELECT subquery3.*, expense_category.first as first_category,expense_category.second as second_category from (\n" +
+            "SELECT subquery2.*, group_fund.total as total_sum from (\n" +
+            "SELECT fund.code as code, subquery1.* from\n" +
+            "(SELECT fund_name as name, group_name as groups, expense_category, SUM(expense) as used_sum  from application where result = 'pass' group by fund_name, group_name, expense_category) as subquery1\n" +
+            "JOIN fund on fund.name = subquery1.name) as subquery2\n" +
+            "JOIN group_fund on group_fund.fund_name = subquery2.name and group_fund.group_name = subquery2.groups) as subquery3\n" +
+            "JOIN expense_category on expense_category.id = subquery3.expense_category")
     List<_AuthorizedFundingDetail> calculateAuthorizedFundingDetail();
 
     @Select("SELECT fund.code as code, fund.name as name, fund.due_date::date as due_date, group_fund.total as total_sum, group_fund.used as used_sum, (group_fund.total - group_fund.used) as left_sum, (100 * group_fund.used / group_fund.total || '%') as current_execution_rate, fund.execution_rate as qualified FROM group_fund JOIN fund ON fund.name = group_fund.fund_name where group_fund.group_name = #{group}")
