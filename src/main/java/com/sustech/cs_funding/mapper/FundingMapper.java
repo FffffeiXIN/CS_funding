ac77629b675ddbbbeb9dbd0b0b424eb654ea7b8e
Siri-Lee <109658172+Siri-Lee@users.noreply.github.com>
2023-05-13 23:44:23 +0800

    Update FundingMapper.java

--- src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
+++ src/main/java/com/sustech/cs_funding/mapper/FundingMapper.java
@@ -30,27 +30,26 @@ public interface FundingMapper {
     @Select("SELECT name AS fund_name, code, due_date, execution_rate FROM fund where name = #{name}")
     Fund getFundByName(String name);
         
-        @Select("SELECT fund.code as code, fund.name as fund_name, fund.due_date::date as due_date,\n" +
+    @Select("SELECT fund.code as code, fund.name as fund_name, fund.due_date::date as due_date,\n" +
             "       SUM(group_fund.total) as total_sum, SUM(group_fund.used) as used_sum,\n" +
             "       (SUM(group_fund.total) - SUM(group_fund.used)) as left_sum,\n" +
             "       (100 * SUM(group_fund.used) / SUM(group_fund.total) || '%') as current_execution_rate,\n" +
+            "       fund.execution_rate as qualified_rate,\n" +
             "       CASE\n" +
             "           when (SUM(group_fund.used) / SUM(group_fund.total)) >= CAST(fund.execution_rate as double precision) then 'Yes'\n" +
             "           else 'No'\n" +
             "       END as qualified\n" +
             "FROM group_fund JOIN fund ON fund.name = group_fund.fund_name group by fund.name")
     List<_MultiUsedTable> calculateFundingSum();
-    
+
     @Select("SELECT group_name, fund_name, code, total, used, (total - used) as usable_left,\n" +
             "       (100 * used / total || '%') as current_execution_rate,\n" +
-            "       fund.execution_rate as qualified_rate,\n" +
             "       CASE\n" +
             "           when (used / total) >= CAST(fund.execution_rate as double precision) then 'Yes'\n" +
             "           else 'No'\n" +
             "       END as qualified\n" +
             "FROM group_fund JOIN fund ON  fund.name = group_fund.fund_name")
     List<_ExpenditureSummary> calculateExpenditureSummary();
-
     @Select("SELECT fund.code as code, fund.name as fund_name, fund.due_date::date as due_date,\n" +
             "       group_fund.total as total_sum, group_fund.used as used_sum,\n" +
             "       (group_fund.total - group_fund.used) as left_sum,\n" +
