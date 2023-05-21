e20af043193a1e86862b81945be527590dde5c13
Siri-Lee <109658172+Siri-Lee@users.noreply.github.com>
2023-05-21 18:22:58 +0800

    Update ManagerMapper.java
    加总金额的判断

--- src/main/java/com/sustech/cs_funding/mapper/ManagerMapper.java
+++ src/main/java/com/sustech/cs_funding/mapper/ManagerMapper.java
@@ -8,6 +8,11 @@ import org.apache.ibatis.annotations.Mapper;
 public interface ManagerMapper {
     @Insert("INSERT INTO group_fund (group_name, fund_name, total, used) VALUES (#{group_name}, #{fund_name}, #{total}, 0)")
     void allocateFundInfo(String group_name, String fund_name, Double total);
+    
+    @Update("UPDATE fund SET total_available = #{total} where code = #{code}")
+    void allocateTotalFundInfo(String code, Double total);
 
+    @Select("SELECT total_available FROM fund where code = #{code}")
+    Double checkTotalFund(String code);
 
 }
